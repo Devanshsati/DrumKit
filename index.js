f5fbc8c894d574c1c18b1651aed747111e54c685
Devanshsati <thedevanshsati@gmail.com>
2021-10-06 19:26:19 +0530

    switch apply

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 for(var i=0;i<document.querySelectorAll(".drum").length;i++){
 	document.querySelectorAll(".drum")[i].addEventListener("click",function(){
 		this.style.color="white";
-		if(this.innerHTML=='w'){
-			var audio= new Audio("sounds/tom-1.mp3");
-			audio.play();
+
+		var buttonInnerHTML = this.innerHTML;
+
+		switch (buttonInnerHTML) {
+			case "w":
+				var tom1= new Audio("sounds/tom-1.mp3");
+				tom1.play();
+			break;
+			case "a":
+				var tom2= new Audio("sounds/tom-2.mp3");
+				tom2.play();
+			break;
+			case "s":
+				var tom3= new Audio("sounds/tom-3.mp3");
+				tom3.play();
+			break;
+			case "d":
+				var tom4= new Audio("sounds/tom-4.mp3");
+				tom4.play();
+			break;
+			case "j":
+				var snare= new Audio("sounds/snare.mp3");
+				snare.play();
+			break;
+			case "k":
+				var kick= new Audio("sounds/kick-bass.mp3");
+				kick.play();
+			break;
+			case "l":
+				var crash= new Audio("sounds/crash.mp3");
+				crash.play();
+			break;
+
+				break;
+			default:
+
 		}
-		else if (this.innerHTML=='a') {
-			var audio= new Audio("sounds/tom-2.mp3");
-			audio.play();
-		}
-		else if (this.innerHTML=='s') {
-			var audio= new Audio("sounds/tom-3.mp3");
-			audio.play();
-		}
-		else if (this.innerHTML=='d') {
-			var audio= new Audio("sounds/tom-4.mp3");
-			audio.play();
-		}
-		else if (this.innerHTML=='j') {
-			var audio= new Audio("sounds/snare.mp3");
-			audio.play();
-		}
-		else if (this.innerHTML=='k') {
-			var audio= new Audio("sounds/kick-bass.mp3");
-			audio.play();
-		}
-		else if (this.innerHTML=='l') {
-			var audio= new Audio("sounds/crash.mp3");
-			audio.play();
-		}
+
 	});
 }
 // var audio= new Audio("sounds/tom-1.mp3");
